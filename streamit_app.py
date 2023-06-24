fd1eabb475f840b9a299c4e6b7d0233eb940af3a
sudhansupatra21 <107807079+sudhansupatra21@users.noreply.github.com>
2023-06-24 12:58:40 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Health Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avacado Toast')
