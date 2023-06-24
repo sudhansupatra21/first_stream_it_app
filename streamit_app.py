737531b9a664af533cdafce704a80fb880303e8f
sudhansupatra21 <107807079+sudhansupatra21@users.noreply.github.com>
2023-06-25 03:12:58 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -23,16 +23,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-   streamlit.error("Please select a fruit to get information.")
-else:
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 except URLError as e:
- streamlit.error()
+    streamlit.error("An error occurred while fetching data. Please try again.")
+
+
 # write your own comment -what does the next line do? 
 
 # write your own comment - what does this do?
