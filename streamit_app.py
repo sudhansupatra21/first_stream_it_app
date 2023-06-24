78d686425f66a5f653a87470e868d5142a683e21
sudhansupatra21 <107807079+sudhansupatra21@users.noreply.github.com>
2023-06-25 00:57:19 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -20,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"Kiwi")
-fruityvice_normalized = panda.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 
