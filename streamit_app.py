4dc3e4b7c362eae5ea9f746c6d92ff963876f638
sudhansupatra21 <107807079+sudhansupatra21@users.noreply.github.com>
2023-06-25 03:25:25 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -21,19 +21,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+#create the repeatale coe block (called function)
+def get_fruityvice_data(fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        data_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(data_from_function)
 except URLError as e:
     streamlit.error("An error occurred while fetching data. Please try again.")
-
-
 # write your own comment -what does the next line do? 
 
 # write your own comment - what does this do?
